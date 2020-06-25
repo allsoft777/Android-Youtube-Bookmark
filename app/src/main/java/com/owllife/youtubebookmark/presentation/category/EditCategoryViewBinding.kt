00098cf80bd3989f252dbb8d665b99197ea48e24
seongil <kims172@gmail.com>
2020-06-26 01:36:54 +0900

    removed some legacy code snippets of data-binding for CategoryListView.

--- app/src/main/java/com/owllife/youtubebookmark/presentation/category/EditCategoryViewBinding.kt
+++ app/src/main/java/com/owllife/youtubebookmark/presentation/category/EditCategoryViewBinding.kt
@@ -1,11 +1,7 @@
 package com.owllife.youtubebookmark.presentation.category
 
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.core.widget.ContentLoadingProgressBar
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.owllife.youtubebookmark.R
 import com.owllife.youtubebookmark.core.gone
 import com.owllife.youtubebookmark.core.visible
 import com.owllife.youtubebookmark.domain.entity.CategoryEntity
@@ -18,31 +14,9 @@ import com.owllife.youtubebookmark.domain.entity.CategoryEntity
 @BindingAdapter("category_listview_items")
 fun setCategoryList(listView: RecyclerView, items: List<CategoryEntity>?) {
     if (items.isNullOrEmpty() || listView.adapter == null) {
+        listView.gone()
         return
     }
     (listView.adapter as CategoryAdapter).submitList(items)
-}
-
-@BindingAdapter(value = ["category_listview_items", "category_listview_loading"], requireAll = true)
-fun setListViewVisibility(
-    view: RelativeLayout, items: List<CategoryEntity>?, isLoading: Boolean
-) {
-    val listView: RecyclerView = view.findViewById(R.id.category_list_view)
-    val emptyTv: TextView = view.findViewById(R.id.empty_text)
-    val loadingView: ContentLoadingProgressBar = view.findViewById(R.id.progressbar)
-    if (isLoading) {
-        loadingView.visible()
-        listView.gone()
-        emptyTv.gone()
-        return
-    }
-    if (items.isNullOrEmpty()) {
-        emptyTv.visible()
-        loadingView.gone()
-        listView.gone()
-        return
-    }
-    emptyTv.gone()
-    loadingView.gone()
     listView.visible()
 }
